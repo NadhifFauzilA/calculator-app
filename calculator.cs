811895325415d4de67182640880954db8f455678
NadhifFauzilA <nadhiffauzil@students.amikom.ac.id>
2023-03-15 12:08:53 +0700

    penamnahan fungsi pengurangan

--- calculator.cs
+++ calculator.cs
@@ -2,11 +2,18 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
-		
+		int a =10;
+		int b=6;
+		console.Writeline("Hasil Penambahan :{0} + {1} = {2} ",a,b, Penambahan (a,b));
 	}
 	static int Penambahan (int a, int b)
 	{
 		return a+b;
+		
+	}
+	static int Pengurangan (int a, int b )
+	{
+		return a-b;
 		
 	}
 }
